95735293d7d860e9024b577f84cac11db8cacf0e
jimmyclarke113 <jimmyclarke89@gmail.com>
2017-06-01 16:49:30 +0100

    Create map.php

---
+++ 3d/map.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <script src="aframe.min.js"></script>
+  </head>
+  <body>
+    <a-scene>
+      <a-assets>
+        <img id="my-texture" src="Mpirical_NetX_2.0_5G - FLAT.png">
+      </a-assets>
+
+      <a-entity id="radio" position="0 0 0" >
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" scale="1 1 1" visible="true" material="color:#FFC65D" ></a-box>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" rotation="0 0 0" scale="" visible="true" material="color:#FFC65D"></a-sphere>
+        <a-cylinder position="-1.38 0.75 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 0" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:cone;radius:0.5;height:3;openEnded:false;thetaStart:0;radiusBottom:0.6;radiusTop:0.2"></a-cylinder>
+        <a-cylinder position="-1.412 3.654 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 120" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:ring;radius:0.5;height:3;openEnded:true;thetaStart:0;radiusBottom:0.6;radiusTop:0.2;skipCache:false;thetaLength:120;segmentsTheta:8;radiusOuter:1"></a-cylinder>
+        <a-cylinder position="-1.412 3.654 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 120" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:ring;radius:0.5;height:3;openEnded:true;thetaStart:0;radiusBottom:0.6;radiusTop:0.2;skipCache:false;thetaLength:120;segmentsTheta:8;radiusOuter:1"></a-cylinder>
+        <a-cylinder position="-1.537 3.654 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 120" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:ring;radius:0.5;height:3;openEnded:true;thetaStart:0;radiusBottom:0.6;radiusTop:0.2;skipCache:false;thetaLength:120;segmentsTheta:8;radiusOuter:0.5;radiusInner:0.4"></a-cylinder>
+        <a-cylinder position="-1.537 3.654 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 120" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:ring;radius:0.5;height:3;openEnded:true;thetaStart:0;radiusBottom:0.6;radiusTop:0.2;skipCache:false;thetaLength:120;segmentsTheta:8;radiusOuter:0.5;radiusInner:0.4"></a-cylinder>
+        <a-cylinder position="-1.38 3.654 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 300" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:ring;radius:0.5;height:3;openEnded:true;thetaStart:0;radiusBottom:0.6;radiusTop:0.2;skipCache:false;thetaLength:120;segmentsTheta:8;radiusOuter:1"></a-cylinder>
+        <a-cylinder position="-1.38 0.75 -7.889" radius="0.5" height="1.5" color="#FFC65D" rotation="0 0 0" scale="1 1 1" visible="true" material="color:#FFC65D" geometry="primitive:cone;radius:0.5;height:3;openEnded:false;thetaStart:0;radiusBottom:0.6;radiusTop:0.2"></a-cylinder>
+      </a-entity>
+
+
+      
+      <!--
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+      -->
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" scale="11 11 11" color="#7BC8A4" material="src: #my-texture" ></a-plane>
+      <a-sky color="#ECECEC"></a-sky>
+    </a-scene>
+  </body>
+</html>
