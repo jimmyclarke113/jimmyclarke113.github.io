cd72b2635ef4c21149423cf57ce0c1feec7ea78e
Jimmy Clarke <jimmyclarke89@gmail.com>
2021-06-23 23:15:16 +0100

    2315

--- vr/vr.js
+++ vr/vr.js
@@ -1,7 +1,8 @@
 AFRAME.registerComponent('grip-button-listener', {
   init: function () {
+    var sceneEl = document.querySelector('a-scene');
     var el = this.el;
-    var sword = this.querySelector('#redBox')
+    var sword = sceneEl.querySelector('#redBox')
     el.addEventListener('abuttondown', function (evt) {
       sword.setAttribute('visible', !sword.getAttribute('visible'));
     });
